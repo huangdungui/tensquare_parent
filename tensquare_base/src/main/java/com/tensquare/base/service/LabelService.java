ed078c60a012cd911821e4c2e389740dfb3b6ddd
huangdungui <huangdungui@163.com>
2019-09-17 16:14:35 +0800

    新增分页查询

--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -3,6 +3,9 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +15,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -39,23 +43,64 @@ public class LabelService {
     public void update(Label label) {
         labelDao.save(label);
     }
+
     public void deleteById(String id) {
         labelDao.deleteById(id);
     }
 
-   public List<Label> findSearch(Label label) {
+    public List<Label> findSearch(Label label) {
         return labelDao.findAll(new Specification<Label>() {
             /**
-             *
-             * @param root 根对象，也就是要把条件封装到那个对象中。
-             * @param criteriaQuery 封装的都是查询关键字，
-             * @param criteriaBuilder 用来封装条件对象
+             * @param root            根对象，也就是要把条件封装到那个对象中。
+             * @param criteriaQuery   封装的都是查询关键字，
+             * @param criteriaBuilder 用来封装条件对象，如果直接返回null，表示不需要条件
              * @return
              */
             @Override
             public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                return null;
+
+                List<Predicate> list = new ArrayList<>();
+                if (label.getLabelname() != null && "".equals(label.getLabelname())) {
+                    Predicate predicate = criteriaBuilder.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
+                    list.add(predicate);
+                }
+                if (label.getState() != null && "".equals(label.getState())) {
+                    Predicate predicate = criteriaBuilder.equal(root.get("status").as(String.class), label.getState());
+                    list.add(predicate);
+                }
+                //new 一个数组作为最终返回值的条件
+                Predicate[] predicates = new Predicate[list.size()];
+
+                predicates = list.toArray(predicates);
+                return criteriaBuilder.and(predicates);
             }
         });
+    }
+
+    public Page<Label> pageQuery(Label label, int page, int size) {
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        return labelDao.findAll(new Specification<Label>() {
+
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+
+                List<Predicate> list = new ArrayList<>();
+                if (label.getLabelname() != null && "".equals(label.getLabelname())) {
+                    Predicate predicate = criteriaBuilder.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
+                    list.add(predicate);
+                }
+                if (label.getState() != null && "".equals(label.getState())) {
+                    Predicate predicate = criteriaBuilder.equal(root.get("status").as(String.class), label.getState());
+                    list.add(predicate);
+                }
+                //new 一个数组作为最终返回值的条件
+                Predicate[] predicates = new Predicate[list.size()];
+
+                predicates = list.toArray(predicates);
+                return criteriaBuilder.and(predicates);
+            }
+        }, pageable);
     }
 }
