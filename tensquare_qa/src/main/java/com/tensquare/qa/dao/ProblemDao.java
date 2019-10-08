7b715909dee0c188dd545f55a6603f390be5a21d
huangdungui <huangdungui@163.com>
2019-10-08 10:11:22 +0800

    新增问答模块

--- tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
@@ -4,11 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.tensquare.qa.pojo.Problem;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 /**
  * 数据访问接口
  * @author Administrator
  *
  */
 public interface ProblemDao extends JpaRepository<Problem,String>,JpaSpecificationExecutor<Problem>{
-	
+    @Query(value = "",nativeQuery = true)
+	public List<Problem> newList();
+	public List<Problem> hotList();
+	public List<Problem> waitList();
 }
