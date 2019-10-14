78d9ada44b21f7295bfe90359026526602755e0d
huangdungui <huangdungui@163.com>
2019-10-14 16:43:19 +0800

    吐槽模块更新

--- tensquare_spit/src/main/java/com/tensquare/spit/dao/SpitDao.java
+++ tensquare_spit/src/main/java/com/tensquare/spit/dao/SpitDao.java
@@ -1,6 +1,8 @@
 package com.tensquare.spit.dao;
 
 import com.tensquare.spit.pojo.Spit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
@@ -11,4 +13,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  **/
 public interface SpitDao extends MongoRepository<Spit,String> {
 
+    public Page<Spit> findByParentid(String parentid, Pageable pageable);
 }
--- tensquare_spit/src/main/java/com/tensquare/spit/service/SpitService.java
+++ tensquare_spit/src/main/java/com/tensquare/spit/service/SpitService.java
@@ -3,10 +3,18 @@ package com.tensquare.spit.service;
 import com.tensquare.spit.dao.SpitDao;
 import com.tensquare.spit.pojo.Spit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import util.IdWorker;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,7 +23,7 @@ import java.util.List;
  * @author: huangdungui
  * @create: 2019-10-11 15:14
  **/
-@ Service
+@Service
 @Transactional
 public class SpitService {
 
@@ -25,7 +33,11 @@ public class SpitService {
     @Autowired
     private IdWorker idWorker;
 
-    public List<Spit> findAll(){
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+
+    public List<Spit> findAll() {
         return spitDao.findAll();
     }
 
@@ -35,6 +47,20 @@ public class SpitService {
 
     public void save(Spit spit) {
         spit.set_id(idWorker.nextId() + "");
+        spit.setPublishtime(new Date());//发布日期
+        spit.setVisits(0);//浏览量
+        spit.setShare(0);//分享数
+        spit.setThumbup(0);//点赞数
+        spit.setComment(0);//回复数
+        spit.setState("1");//状态
+        //如果点前添加的吐槽，有父节点，那么父节点的吐槽回复数要加一
+        if (spit.getParentid() != null && "".equals(spit.getParentid())) {
+            Query query = new Query();
+            query.addCriteria(Criteria.where("_id").is(spit.getParentid()));
+            Update update = new Update();
+            update.inc("comment", 1);
+            mongoTemplate.updateFirst(query, update, "spit");
+        }
         spitDao.save(spit);
     }
 
@@ -44,5 +70,24 @@ public class SpitService {
 
     public void deleteById(String id) {
         spitDao.deleteById(id);
+    }
+
+    public Page<Spit> findByParentid(String parentid, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return spitDao.findByParentid(parentid, pageable);
+    }
+
+    public void thumbup(String spitId) {
+        //方式一：效率有问题
+        /*Spit spit = spitDao.findById(spitId).get();
+        spit.setThumbup((spit.getThumbup() == null ? 0 : spit.getThumbup()) + 1);
+        spitDao.save(spit);*/
+
+        //方式二：原生的mongo命令来实现自增
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is("1"));
+        Update update = new Update();
+        update.inc("thumbup", 1);
+        mongoTemplate.updateFirst(query, update, "spit");
     }
 }
